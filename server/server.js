6e91b4c6b5d9a490f1890d51a35daf658c355023
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-28 23:36:22 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+// Routes
+
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+  });
+  
+module.exports = {app};
