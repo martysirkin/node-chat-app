484cfd19ba793f8f6fc313c68ebfb406171770c7
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-29 11:54:37 -0400

    Add connect disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,20 +1,34 @@
 //const _ = require('lodash');
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-var app = express();
-
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
 
 // Routes
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
   });
   
-module.exports = {app};
+//module.exports = {app};
