707574082ee7d9fde9fa8e60475ec7f5393a0c72
Marty Sirkin <martysirkin@Martys-MacBook-Pro.local>
2018-07-30 10:35:20 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,14 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -42,12 +45,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by this browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send location...');
      }, function () {
+        locationButton.removeAttr('disabled').text('Send location...');
         alert('Unable to fetch location.');
     });
 });
